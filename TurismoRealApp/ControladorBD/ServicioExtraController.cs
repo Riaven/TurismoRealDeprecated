b92b9cff709fca817922cf8f90261fdb99b8aae7
Riaven <jogubaez@hotmail.com>
2020-10-03 23:58:08 -0300

    add eliminar, modificar en ServicioExtraController

--- TurismoRealApp/ControladorBD/ServicioExtraController.cs
+++ TurismoRealApp/ControladorBD/ServicioExtraController.cs
@@ -123,12 +123,83 @@ namespace ControladorBD
             return creado;
         }
         //Llamada a modificar
+        public static int ModificarServicioExtra(int id, string descripcion)
+        {
+            conn = ConexionBD.AbrirConexion();
+            int modificado = 0;
+            try
+            {
+                OracleCommand cmd = new OracleCommand("SP_MODIFICAR_SERVICIO_EXTRA", conn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                //tomando los datos
+                //p_id parámetro de entrada y salida, contiene el id 
+                OracleParameter id_inout = new OracleParameter();
+                id_inout.ParameterName = "P_ID";
+                id_inout.OracleDbType = OracleDbType.Decimal;
+                id_inout.Direction = ParameterDirection.InputOutput;
+                id_inout.Value = id;
+                cmd.Parameters.Add(id_inout);
+
+                //p_id parámetro de entrada y salida, contiene el id 
+                OracleParameter descripcion_in = new OracleParameter();
+                descripcion_in.ParameterName = "P_DESCRIPCION";
+                descripcion_in.OracleDbType = OracleDbType.Varchar2;
+                descripcion_in.Size = 20;
+                descripcion_in.Direction = ParameterDirection.Input;
+                descripcion_in.Value = descripcion;
+                cmd.Parameters.Add(descripcion_in);
+
+                cmd.ExecuteNonQuery();
+
+                modificado = int.Parse(id_inout.Value.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Houston, tenemos un problema : {ex}");
+                modificado = -1;
+            }
+            //-1 cuando se tenga un problema con conexionbd
+            // 0 cuando no se encontró con el id solicitado, no fue posible modificar
+            // x>0 cuando si encuentre, entonces es posible modificar
+            return modificado;
+        }
+
         //Llamada a eliminar
+        public static int EliminarServicioExtra(int id)
+        {
+            conn = ConexionBD.AbrirConexion();
+            int eliminado = 0;
+            try
+            {
+                OracleCommand cmd = new OracleCommand("SP_ELIMINAR_SERVICIO_EXTRA", conn);
 
+                cmd.CommandType = CommandType.StoredProcedure;
+                //tomando los datos
+                //p_id parámetro de entrada y salida, contiene el id 
+                OracleParameter id_inout = new OracleParameter();
+                id_inout.ParameterName = "P_ID";
+                id_inout.OracleDbType = OracleDbType.Decimal;
+                id_inout.Direction = ParameterDirection.InputOutput;
+                id_inout.Value = id;
+                cmd.Parameters.Add(id_inout);
 
+                cmd.ExecuteNonQuery();
 
+                eliminado = int.Parse(id_inout.Value.ToString());
 
-
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Houston, tenemos un problema : {ex}");
+                eliminado = -1;
+            }
+            //-1 cuando se tenga un problema con conexionbd
+            // 0 cuando no se encontró con el id solicitado, no fue posible eliminar
+            // x>0 cuando si encuentre, entonces es posible eliminar
+            return eliminado; 
+        }
 
     }
 }
